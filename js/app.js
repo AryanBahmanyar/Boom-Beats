e73c3b2b5d9bbc9ee7f1bb697b7fa48992246fb6
AryanBahmanyar <141065358+AryanBahmanyar@users.noreply.github.com>
2024-04-30 23:58:59 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,7 @@
-/**
- * WEB222 – Assignment 04
- *
- * I declare that this assignment is my own work in accordance with
- * Seneca Academic Policy. No part of this assignment has been
- * copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Please update the following with your information:
- *
- *      Name:       Aryan Bahmanyar
- *      Student ID: 102976198
- *      Date:       03/20/2024
- */
-
 // All of our data is available on the global `window` object.
 // Create local variables to work with it in this file.
 const { artists, songs } = window;
 
-// For debugging, display all of our data in the console. You can remove this later.
 console.log({ artists, songs }, "App Data");
 
 window.addEventListener("DOMContentLoaded", () => {
